8dcd29b3195625223ccb0dc8ebdd777ca27c80e7
bdj.dreamer <jkserena@naver.com>
2024-06-21 13:00:52 +0900

    normalization

---
+++ src/day18/homework/Member.java
@@ -0,0 +1,12 @@
+package day18.homework;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Member {
+
+	private String name;
+	private String id;
+
+}
---
+++ src/day18/homework/Schedule.java
@@ -0,0 +1,13 @@
+package day18.homework;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Schedule {
+	
+	private String date;
+	private String content;
+	private String details;
+
+}
