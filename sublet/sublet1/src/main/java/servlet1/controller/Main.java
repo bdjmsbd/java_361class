362e105cb1a655d9d1cc43070d9673c80c9fb9dc
bdj.dreamer <jkserena@naver.com>
2024-08-07 15:51:00 +0900

    sublet 1일차 중간.

---
+++ sublet/sublet1/src/main/java/servlet1/controller/Main.java
@@ -0,0 +1,26 @@
+package servlet1.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Main extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Hello. 안녕하세요. 뚜데끼야");
+		request.getRequestDispatcher("/WEB-INF/views/main.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ sublet/sublet1/src/main/java/servlet1/filter/EncodingFilter.java
@@ -0,0 +1,38 @@
+package servlet1.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/")
+public class EncodingFilter extends HttpFilter implements Filter {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		// 화면에서 서버로 전송할 때
+		String encoding = "UTF-8";
+		request.setCharacterEncoding(encoding);	
+		
+		// 서버에서 화면으로 전송할 때
+		response.setCharacterEncoding(encoding);
+		response.setContentType("text/html; charset=" + encoding);
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+}
