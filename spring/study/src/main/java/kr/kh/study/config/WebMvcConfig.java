85231faf4e7ab58be8e726604c073afaee13fdad
bdj.dreamer <jkserena@naver.com>
2024-09-20 14:36:13 +0900

    config 분리하기
    @bean 어노테이션,
    클래스 만들어서 @configuration 등록하면 동일하게 구현

--- spring/study/src/main/java/kr/kh/study/config/WebMvcConfig.java
+++ spring/study/src/main/java/kr/kh/study/config/WebMvcConfig.java
@@ -3,15 +3,11 @@ package kr.kh.study.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
-import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 import kr.kh.study.interceptor.MemberInterceptor;
 
@@ -32,24 +28,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-
-	// TilesViewResolver 설정
-	@Bean
-	public TilesViewResolver tilesViewResolver() {
-		TilesViewResolver tilesViewResolver = new TilesViewResolver();
-		tilesViewResolver.setOrder(1); // ViewResolver의 우선순위 설정
-		return tilesViewResolver;
-	}
-
-	// Tiles 설정
-	@Bean
-	public TilesConfigurer tilesConfigurer() {
-		TilesConfigurer tilesConfigurer = new TilesConfigurer();
-		tilesConfigurer.setDefinitions("/WEB-INF/spring/tiles.xml");
-		tilesConfigurer.setCheckRefresh(true); // 변경 사항을 자동으로 감지하여 갱신
-		return tilesConfigurer;
-	}
-
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// 인터셉터 추가 및 URL 패턴 설정
@@ -57,8 +36,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
 				.excludePathPatterns("/post/list", "/post/detail"); // 제외할 경로 // 특정 경로 제외
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(); // BCryptPasswordEncoder 빈 등록
-	}
 }
--- spring/study/src/main/java/kr/kh/study/controller/HomeController.java
+++ spring/study/src/main/java/kr/kh/study/controller/HomeController.java
@@ -1,18 +1,18 @@
 package kr.kh.study.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
+
+import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.kh.study.dao.PostDAO;
 
@@ -23,6 +23,9 @@ import kr.kh.study.dao.PostDAO;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private JavaMailSender mailSender;
 	
 	@Autowired
 	private PostDAO postDao;
@@ -43,6 +46,8 @@ public class HomeController {
 		
 		System.out.println("enc : "+ enc);
 		System.out.println("res : "+ res);
+		
+		//mailSend("jkserena@naver.com","메일 테스티", "전송이 잘 됐습니다.");
 
 		return "/home";
 	}
@@ -65,4 +70,27 @@ public class HomeController {
 		return "/home";
 	}
 	
+
+
+	public boolean mailSend(String to, String title, String content) {
+
+	    String setfrom = "stajun@naver.com";
+	    
+	   try{
+	        MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper messageHelper
+	            = new MimeMessageHelper(message, true, "UTF-8");
+
+	        messageHelper.setFrom(setfrom);// 보내는사람 생략하거나 하면 정상작동을 안함
+	        messageHelper.setTo(to);// 받는사람 이메일
+	        messageHelper.setSubject(title);// 메일제목은 생략이 가능하다
+	        messageHelper.setText(content, true);// 메일 내용
+
+	        mailSender.send(message);
+	        return true;
+	    } catch(Exception e){
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
 }
